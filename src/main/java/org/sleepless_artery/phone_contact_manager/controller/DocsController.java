85f93cc7327fe1d72ea8745d6457af01c1ee6610
ART3RY-G1N <cplusplus3414@gmail.com>
2025-11-18 16:19:38 +0300

    Added documentation controller and template

---
+++ src/main/java/org/sleepless_artery/phone_contact_manager/controller/DocsController.java
@@ -0,0 +1,15 @@
+package org.sleepless_artery.phone_contact_manager.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class DocsController {
+
+    @GetMapping("/docs")
+    public String docs() {
+        return "docs";
+    }
+}
+
